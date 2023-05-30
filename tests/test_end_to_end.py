f98f0df07ca56268544a9b7d52b2d69951ecee08
Vitaliy Mysak <vmysak@bccfe.ca>
2023-05-30 14:00:57 -0700

    test: simplify argument passing

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -5,17 +5,6 @@ import filecmp
 import os
 import intact.intact as main
 
-
-@pytest.fixture(scope="session")
-def small_data_file(tmpdir_factory):
-    return "tests/small.fasta"
-
-
-@pytest.fixture(scope="session")
-def large_data_file(tmpdir_factory):
-    return "tests/data.fasta"
-
-
 def run_end_to_end(tmp_path, data_file, expected_dir):
     main.intact(
         working_dir=tmp_path,
@@ -36,11 +25,11 @@ def run_end_to_end(tmp_path, data_file, expected_dir):
     assert result.common == result.right_list
 
 
-def test_small(tmp_path, small_data_file):
-    run_end_to_end(tmp_path, small_data_file, "./tests/expected-results-small")
+def test_small(tmp_path):
+    run_end_to_end(tmp_path, "tests/data-small.fasta", "./tests/expected-results-small")
 
 
 @pytest.mark.slow
-def test_large(tmp_path, large_data_file):
-    run_end_to_end(tmp_path, large_data_file, "./tests/expected-results-large")
+def test_large(tmp_path):
+    run_end_to_end(tmp_path, "tests/data-large.fasta", "./tests/expected-results-large")
 
