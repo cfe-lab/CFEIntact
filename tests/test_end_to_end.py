2c4ed186dbd8bb0dd96fefa6b481c77b5a217fa9
Vitaliy Mysak <vmysak@bccfe.ca>
2023-06-06 10:06:20 -0700

    test: enable nonhiv, scramble and internal inversion checks

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -16,9 +16,9 @@ def run_end_to_end(tmp_path, data_file, expected_dir):
         check_major_splice_donor_site=True,
         run_hypermut=True,
         check_long_deletion=True,
-        check_nonhiv=False,
-        check_scramble=False,
-        check_internal_inversion=False,
+        check_nonhiv=True,
+        check_scramble=True,
+        check_internal_inversion=True,
         include_small_orfs=False,
         )
 
