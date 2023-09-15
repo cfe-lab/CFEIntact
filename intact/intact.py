743d9a4ed41f084afb2cebd6b1d909aa8fbbbf52
Vitaliy Mysak <vmysak@bccfe.ca>
2023-09-15 16:41:51 -0700

    prevent index out of bounds exceptions

--- intact/intact.py
+++ intact/intact.py
@@ -535,7 +535,7 @@ def has_reading_frames(
         expected_nucleotides = e.nucleotides
         expected_aminoacids = e.aminoacids
         q_start = coordinates_mapping[e.start]
-        q_end = coordinates_mapping[e.end]
+        q_end = coordinates_mapping[e.end - 1 if e.end == len(coordinates_mapping) else e.end]
         got_nucleotides = sequence.seq[q_start:q_end]
         got_aminoacids = translate(got_nucleotides)
         q_start_a = q_start // 3
@@ -558,7 +558,7 @@ def has_reading_frames(
 
     def find_real_correspondence(e):
         q_start = coordinates_mapping[e.start]
-        q_end = coordinates_mapping[e.end]
+        q_end = coordinates_mapping[e.end - 1 if e.end == len(coordinates_mapping) else e.end]
         candidates = list(find_candidate_positions(e, q_start, q_end))
         return min(candidates, key=lambda x: x.distance)
 
