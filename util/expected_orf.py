8e7e2d470079a64b76a4660e69d20e11938ebc6f
Vitaliy Mysak <vmysak@bccfe.ca>
2023-09-22 16:25:40 -0700

    Use find_orf for ExpectedORF initialization

--- util/expected_orf.py
+++ util/expected_orf.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from util.reference_index import ReferenceIndex
 from util.translate_to_aminoacids import translate_to_aminoacids
-from util.get_biggest_protein import get_biggest_protein
+from util.find_orf import find_orf
 
 
 @dataclass
@@ -14,23 +13,23 @@ class ExpectedORF:
     aminoacids: str
     protein: str
 
-
     @staticmethod
     def subtyped(aligned_sequence, name, start, end, deletion_tolerence):
-        start_s = ReferenceIndex(start).mapto(aligned_sequence)
-        end_s = ReferenceIndex(end).mapto(aligned_sequence)
-
-        nucleotides = str(aligned_sequence.this.seq[start_s:(end_s + 1)])
+        nucleotides = aligned_sequence.reference.seq[start:(end + 1)]
         aminoacids = translate_to_aminoacids(nucleotides)
-        has_start_codon = translate_to_aminoacids(aligned_sequence.reference.seq[start:(end + 1)]).startswith("M")
-        protein = get_biggest_protein(has_start_codon, aminoacids)
+        protein = aminoacids.strip("*")
+        reference_orf = \
+            ExpectedORF(
+                name=name,
+                start=start,
+                end=end,
+                deletion_tolerence=deletion_tolerence,
+                nucleotides=nucleotides,
+                aminoacids=aminoacids,
+                protein=protein,
+            )
 
-        return ExpectedORF(name=name,
-                           start=start_s,
-                           end=end_s,
-                           deletion_tolerence=deletion_tolerence,
-                           nucleotides=nucleotides,
-                           aminoacids=aminoacids,
-                           protein=protein,
-                           )
+        subtype_orf = find_orf(aligned_sequence, reference_orf)
+        subtype_orf.deletion_tolerence = deletion_tolerence
 
+        return subtype_orf
