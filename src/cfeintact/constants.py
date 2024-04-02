72e9364f65f55028c5a0b73ea159d0401042b11f
Vitaliy Mysak <vmysak@bccfe.ca>
2024-04-02 16:21:38 -0700

    Adjust distance tolerance

--- src/cfeintact/constants.py
+++ src/cfeintact/constants.py
@@ -14,19 +14,19 @@ ORFDefinition = Tuple[str, int, int, int, int, Fraction]
 ORFsDefinition = List[ORFDefinition]
 
 DEFAULT_FORWARD_ORFs: ORFsDefinition \
-    = [("gag", 790, 2292, 39, 201, Fraction("0.66")),
-       ("pol", 2085, 5096, 91, 21, Fraction("0.76")),
-       ("env", 6225, 8795, 54, 363, Fraction("0.69"))]
+    = [("gag", 790, 2292, 39, 201, Fraction("0.5981496276061171")),
+       ("pol", 2085, 5096, 91, 21, Fraction("0.7442043222003922")),
+       ("env", 6225, 8795, 54, 363, Fraction("0.6303960531725368"))]
 DEFAULT_REVERSE_ORFS: ORFsDefinition = []
 DEFAULT_SMALL_FORWARD_ORFS: ORFsDefinition \
-    = [("vif", 5041, 5619, 3, 3, Fraction("0.69")),
-       ("vpr", 5559, 5850, 6, 3, Fraction("0.50")),
-       ("tat_exon1", 5831, 6045, 0, 0, Fraction("0.61")),
-       ("rev_exon1", 5970, 6045, 0, 0, Fraction("0.60")),
-       ("vpu", 6062, 6310, 6, 24, Fraction("0.75")),
-       ("tat_exon2", 8377, 8469, 0, 3, Fraction("0.70")),
-       ("rev_exon2", 8378, 8653, 6, 3, Fraction("0.61")),
-       ("nef", 8797, 9417, 48, 51, Fraction("0.68"))]
+    = [("vif", 5041, 5619, 3, 3, Fraction("0.6257074581813602")),
+       ("vpr", 5559, 5850, 6, 3, Fraction("0.4742978272390036")),
+       ("tat_exon1", 5831, 6045, 0, 0, Fraction("0.5616889405384466")),
+       ("rev_exon1", 5970, 6045, 0, 0, Fraction("0.6713780918727915")),
+       ("vpu", 6062, 6310, 6, 24, Fraction("0.5982053838484547")),
+       ("tat_exon2", 8377, 8469, 0, 3, Fraction("0.5396509491733008")),
+       ("rev_exon2", 8378, 8653, 6, 3, Fraction("0.6868760338334845")),
+       ("nef", 8797, 9417, 48, 51, Fraction("0.6840688395346594"))]
 DEFAULT_SMALL_REVERSE_ORFS: ORFsDefinition = []
 
 DEFAULT_ORF_LENGTH = 1000
