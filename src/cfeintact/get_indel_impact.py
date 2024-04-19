53b705ee26e03aadec9b9626503a82e41f5d1eed
Vitaliy Mysak <vmysak@bccfe.ca>
2024-04-19 15:17:54 -0700

    Simplify get_indel_impact

--- src/cfeintact/get_indel_impact.py
+++ src/cfeintact/get_indel_impact.py
@@ -6,22 +6,14 @@ T = TypeVar("T")
 def get_indel_impact(reference: Iterable[T], query: Iterable[T]) -> int:
     shift = 0
     impacted = 0
-    counter = 0
-    good = 0
 
     for (x, y) in zip(reference, query):
         if x == "-" and y != "-":
             shift += 1
         if x != "-" and y == "-":
             shift -= 1
-        if shift % 3 != 0:
-            counter += 1
-            good = 0
-        else:
-            good += 1
-            if good > 5:
-                impacted += counter if counter > 30 else 0
-                counter = 0
 
-    impacted += counter if counter > 30 else 0
+        if shift % 3 != 0:
+            impacted += 1
+
     return impacted
