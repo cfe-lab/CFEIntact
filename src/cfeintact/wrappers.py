3fba957da89ec99fc30907da5fbfdb2a4cd9b189
Vitaliy Mysak <vmysak@bccfe.ca>
2024-07-31 11:03:48 -0700

    Catch keyboard interrupts in wrappers

--- src/cfeintact/wrappers.py
+++ src/cfeintact/wrappers.py
@@ -32,6 +32,8 @@ def mafft(sequences: Iterable[SeqRecord]) -> MultipleSeqAlignment:
             raise UserError("MAFFT run failed with %s. "
                             "Please check if the input FASTA file is correctly formatted.",
                             e) from e
+        except KeyboardInterrupt as e:
+            raise UserError("MAFFT run got cancelled. Cannot continue the analysis.") from e
 
         alignment: MultipleSeqAlignment = AlignIO.read(alignment_output.name, "fasta")
         return alignment
@@ -64,6 +66,8 @@ def blast(alignment_file: str, input_file: str, output_file: str) -> None:
             raise UserError("BLAST run failed with %s. "
                             "Please check if the input FASTA file is correctly formatted.",
                             e) from e
+        except KeyboardInterrupt as e:
+            raise UserError("BLAST run got cancelled. Cannot continue the analysis.") from e
 
         output.write(','.join(fields) + '\n')
         alignment_output.seek(0)
