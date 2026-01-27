481500c05973a2c86a4abf611cdacf1656f233ee
name <email>
2026-01-27 22:54:06 +0000

    Refactor postcreate.sh to install system packages and uv package manager

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
-python3 -m pip install .[dev]
-python3 -m pip install .[test]
-python3 -m pip uninstall -y cfeintact
+set -xe
+
+# Install necessary system packages.
+apt-get install -y git curl tar
+
+# Install python's uv package manager.
+curl -LsSf https://astral.sh/uv/install.sh > /tmp/install_uv.sh
+sh /tmp/install_uv.sh
+
+uv sync --all-etras --refresh
+uv run cfeintact version
